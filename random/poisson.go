669a9fbee9298fd0f05bd50ada1ffad188aa4e0d
Mingzhi Lin <mingzhi9@gmail.com>
2012-08-23 17:52:16 -0400

    change commands

--- random/poisson.go
+++ random/poisson.go
@@ -1,24 +1,22 @@
-/*
- *   Copyright (C) 2012 Mingzhi Lin
- *
- * Permission is hereby granted, free of charge, to any person obtaining 
- * a copy of this software and associated documentation files (the "Software"), 
- * to deal in the Software without restriction, including without limitation 
- * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
- * and/or sell copies of the Software, and to permit persons to whom the 
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included 
- * in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
- * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
- * SOFTWARE.
- */
+//   Copyright (C) 2012 Mingzhi Lin
+//
+// Permission is hereby granted, free of charge, to any person obtaining 
+// a copy of this software and associated documentation files (the "Software"), 
+// to deal in the Software without restriction, including without limitation 
+// the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+// and/or sell copies of the Software, and to permit persons to whom the 
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included 
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+// SOFTWARE.
 
 package random
 
@@ -81,16 +79,15 @@ func (poisson *Poisson) Seed(seed int64) {
 	poisson.randGenerator.Seed(seed)
 }
 
-/* Int returns a non-negative pseudo-random int from a poisson distribution.
- * Implementation: High performance implementation.
- * Patchwork Rejection/Inversion method.
- * This is a port of Poisson.java from the colt java library, which is based upon
- * H. Zechner (1994): Efficient sampling from continuous and discrete unimodal distributions,
- * Doctoral Dissertation, 156 pp., Technical University Graz, Austria.
- * Also see
- * Stadlober E., H. Zechner (1999), The patchwork rejection method for sampling from unimodal distributions,
- * to appear in ACM Transactions on Modelling and Simulation.
- */
+// Int returns a non-negative pseudo-random int from a poisson distribution.
+// Implementation: High performance implementation.
+// Patchwork Rejection/Inversion method.
+// This is a port of Poisson.java from the colt java library, which is based upon
+// H. Zechner (1994): Efficient sampling from continuous and discrete unimodal distributions,
+// Doctoral Dissertation, 156 pp., Technical University Graz, Austria.
+// Also see
+// Stadlober E., H. Zechner (1999), The patchwork rejection method for sampling from unimodal distributions,
+// to appear in ACM Transactions on Modelling and Simulation.
 func (poisson *Poisson) Int() (k int) {
 	/******************************************************************
 	 *                                                                *
