e700b9b47ca4ed4f8d65f75c81e0958a5448aa46
Mingzhi Lin <mingzhi9@gmail.com>
2014-12-09 11:46:09 -0500

    should return NaN if n is small than 0

--- stat/correlation/covariance.go
+++ stat/correlation/covariance.go
@@ -5,10 +5,10 @@ import (
 )
 
 type BivariateCovariance struct {
-	meanX, meanY float64 // the mean of variable x and y
-	n int64 // number of observations
-	estimator float64 // the running covariance estimate
-	biasCorrected bool // flag for bias correction
+	meanX, meanY  float64 // the mean of variable x and y
+	n             int64   // number of observations
+	estimator     float64 // the running covariance estimate
+	biasCorrected bool    // flag for bias correction
 }
 
 func NewBivariateCovariance(biasCorrected bool) *BivariateCovariance {
@@ -23,7 +23,7 @@ func (cov *BivariateCovariance) Increment(x, y float64) {
 	deltaY := y - cov.meanY
 	cov.meanX += deltaX / float64(cov.n)
 	cov.meanY += deltaY / float64(cov.n)
-	cov.estimator += ((float64(cov.n) - 1.0)/float64(cov.n)) * deltaX * deltaY
+	cov.estimator += ((float64(cov.n) - 1.0) / float64(cov.n)) * deltaX * deltaY
 }
 
 func (cov *BivariateCovariance) Append(cov2 *BivariateCovariance) {
@@ -31,9 +31,9 @@ func (cov *BivariateCovariance) Append(cov2 *BivariateCovariance) {
 	cov.n += cov2.n
 	deltaX := cov2.meanX - cov.meanX
 	deltaY := cov2.meanY - cov.meanY
-	cov.meanX += deltaX * float64(cov2.n)/float64(cov.n)
-	cov.meanY += deltaY * float64(cov2.n)/float64(cov.n)
-	cov.estimator += cov2.estimator + float64(oldN) * float64(cov2.n) / float64(cov.n) * deltaX * deltaY
+	cov.meanX += deltaX * float64(cov2.n) / float64(cov.n)
+	cov.meanY += deltaY * float64(cov2.n) / float64(cov.n)
+	cov.estimator += cov2.estimator + float64(oldN)*float64(cov2.n)/float64(cov.n)*deltaX*deltaY
 }
 
 func (cov *BivariateCovariance) GetN() int {
@@ -45,7 +45,7 @@ func (cov *BivariateCovariance) GetResult() float64 {
 	if cov.biasCorrected {
 		n = n - 1
 	}
-	if n == 0 {
+	if n <= 0 {
 		return math.NaN()
 	} else if n == 1 {
 		return 0
